25ef2f4aa43c7414d88af54bf8f25fb81ee3bb8c
Anish202020 <anishkumarbdmi@gmail.com>
2023-11-13 21:40:11 +0530

    first stack commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,16 @@
-import LoginForm from '@/components/LoginForm'
-import Image from 'next/image'
+import LoginForm from "@/components/LoginForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session) redirect("/home");
+
   return (
     <main>
-      <LoginForm></LoginForm>
+      <LoginForm />
     </main>
-  )
+  );
 }
